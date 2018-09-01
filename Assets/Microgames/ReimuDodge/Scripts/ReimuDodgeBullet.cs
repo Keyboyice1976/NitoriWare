95ea4ebdfdbc0d5687099796101289606f3635f6
Scarlex <keyboyice1976@gmail.com>
2018-09-01 11:21:01 -0700

    changes
    added bullet traits (incomplete)

---
+++ Assets/Microgames/ReimuDodge/Scripts/ReimuDodgeBullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Creates prompt for unity's variables
+[Header("The thing to fly towards")]
+[SerializeField]
+private GameObject target;
+//Direction of Bullet's travel
+private Vector2 trajectory;
+public class ReimuDodgeBullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //Finds the trajectory towards Reimu
+        trajectory = (target.transform.position - transform.position).normalized;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
